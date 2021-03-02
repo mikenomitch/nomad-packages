11e94897f9872a8fb1b2a8fd52d55c1820a7a82a
Mike Nomitch <mnomitch@hashicorp.com>
2021-03-01 23:09:19 -0800

    Messing around

--- leak/variables.tf
+++ leak/variables.tf
@@ -1,16 +1,16 @@
 variable "datacenters" {
   description = "the datacenters used for the job"
-  type        = "string"
+  type        = string
 }
 
 variable "cpu" {
   description = "cpu used for the task"
-  type        = "number"
+  type        = number
   default     = 200
 }
 
 variable "memory" {
   description = "memory used for the task"
-  type        = "number"
+  type        = number
   default     = 200
 }
